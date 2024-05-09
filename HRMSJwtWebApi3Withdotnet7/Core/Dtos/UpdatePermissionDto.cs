704f350793fb5ae1793221e0ec0ad407cb65dfb6
haritzismaildev <haritz.ismail.dev@gmail.com>
2024-05-10 05:34:05 +0700

    Add Dtos and static Roles

---
+++ HRMSJwtWebApi3Withdotnet7/Core/Dtos/UpdatePermissionDto.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HRMSJwtWebApi3Withdotnet7.Core.Dtos
+{
+    public class UpdatePermissionDto
+    {
+        [Required(ErrorMessage = "Username is required")]
+        public string Username { get; set; }
+    }
+}
